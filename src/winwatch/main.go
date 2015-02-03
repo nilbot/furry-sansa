9750d2e28c024ac97fe2992bd2e7050e197d9ec6
Ersi Ni <ersi.ni@nilbot.net>
2015-02-03 22:36:59 +0100

    eaburns' Watch not working on windows. built a go test watcher on windows using fsnotify.v1 api

---
+++ src/winwatch/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/fsnotify.v1"
+	"log"
+	"os/exec"
+)
+
+func main() {
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				//log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					cmd := exec.Command("go", "test")
+					out, _ := cmd.CombinedOutput()
+					fmt.Printf("%s\n", out)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
